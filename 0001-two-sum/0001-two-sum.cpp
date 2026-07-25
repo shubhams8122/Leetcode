894b0f6279dd1fcab1e942f3ca70b58efa947c36
Shubham Singh <shubhamsingh92275@gmail.com>
2026-07-25 22:25:11 +0530

    Time: 35 ms (34.9%), Space: 14 MB (83.15%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,37 @@
+	#include <vector>
+
+	using namespace std;
+
+	 
+
+	class Solution {
+
+	public:
+
+	    vector<int> twoSum(vector<int>& nums, int target) {
+
+	        int n = nums.size();
+
+	        for (int i = 0; i < n; ++i) {
+
+	            for (int j = i + 1; j < n; ++j) {
+
+	                if (nums[i] + nums[j] == target) {
+
+	                    return {i, j};
+
+	                }
+
+	            }
+
+	        }
+
+	        return {}; // If no solution found
+
+	    }
+
+	};
+
+	// Time: O(n^2)
+
+	// Space: O(1)
